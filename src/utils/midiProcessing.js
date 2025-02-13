3d66f76c6e7fba25fd8d460b66eb486284d7a79c
Brahamanbtp <brahamanbtp@gmail,com>
2025-02-13 19:13:30 +0530

    midiProcessing.js updated

---
+++ src/utils/midiProcessing.js
@@ -0,0 +1,78 @@
+// Function to convert MIDI data to a note sequence
+export const midiToNoteSequence = (midiData) => {
+    const noteSequence = [];
+  
+    midiData.tracks.forEach((track) => {
+      let time = 0;
+      track.forEach((event) => {
+        time += event.deltaTime;
+        if (event.type === 'noteOn' && event.velocity > 0) {
+          noteSequence.push({
+            note: event.noteName,
+            time: time,
+            velocity: event.velocity,
+          });
+        }
+      });
+    });
+  
+    return noteSequence;
+  };
+  
+  // Function to quantize MIDI events to the nearest beat
+  export const quantizeMidiEvents = (midiData, quantizeValue = 16) => {
+    const quantizedData = { ...midiData, tracks: [] };
+  
+    midiData.tracks.forEach((track) => {
+      const quantizedTrack = [];
+      let time = 0;
+  
+      track.forEach((event) => {
+        time += event.deltaTime;
+        const quantizedTime = Math.round(time / quantizeValue) * quantizeValue;
+        quantizedTrack.push({
+          ...event,
+          deltaTime: quantizedTime - (time - event.deltaTime),
+        });
+        time = quantizedTime;
+      });
+  
+      quantizedData.tracks.push(quantizedTrack);
+    });
+  
+    return quantizedData;
+  };
+  
+  // Function to transpose MIDI notes by a specified interval
+  export const transposeMidiNotes = (midiData, interval = 0) => {
+    const transposedData = { ...midiData, tracks: [] };
+  
+    midiData.tracks.forEach((track) => {
+      const transposedTrack = track.map((event) => {
+        if (event.type === 'noteOn' || event.type === 'noteOff') {
+          return {
+            ...event,
+            noteNumber: event.noteNumber + interval,
+          };
+        }
+        return event;
+      });
+  
+      transposedData.tracks.push(transposedTrack);
+    });
+  
+    return transposedData;
+  };
+  
+  // Function to filter MIDI events by type
+  export const filterMidiEvents = (midiData, eventType) => {
+    const filteredData = { ...midiData, tracks: [] };
+  
+    midiData.tracks.forEach((track) => {
+      const filteredTrack = track.filter((event) => event.type === eventType);
+      filteredData.tracks.push(filteredTrack);
+    });
+  
+    return filteredData;
+  };
+  
